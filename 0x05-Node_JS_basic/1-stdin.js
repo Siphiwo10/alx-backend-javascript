2f2b1ef7bee077b45b098497bd19b9bf24ccdd83
Siphiwo10 <siphiwoj10@gmail.com>
2024-11-29 20:25:39 +0000

    semi fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,23 +3,23 @@ const readline = require('readline');
 
 // Creating an interface to read user input from stdin and write output to stdout
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
 // Display the initial prompt message
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
 rl.on('line', (input) => {
-    // Display the user's name
-    console.log(`Your name is: ${input}`);
-    // Display the closing message
-    console.log("This important software is now closing");
-    // Close the readline interface
-    rl.close();
+  // Display the user's name
+  console.log(`Your name is: ${input}`);
+  // Display the closing message
+  console.log('This important software is now closing');
+  // Close the readline interface
+  rl.close();
 });
 
 rl.on('close', () => {
-    // Ensure the closing message is followed by a new line
-    process.exit(0);
+  // Ensure the closing message is followed by a new line
+  process.exit(0);
 });
